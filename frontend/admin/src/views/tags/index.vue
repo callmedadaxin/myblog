6983b7f139a6538f149f21b73785316d86a78c89
callmedadaxin <554091944@qq.com>
2017-04-13 18:09:07 +0800

    complete type management

--- frontend/admin/src/views/tags/index.vue
+++ frontend/admin/src/views/tags/index.vue
@@ -9,7 +9,7 @@
         v-for="tag in tagList"
         :closable="true"
         :close-transition="false"
-        @close="handleClose(tag)"
+        @close="handleClose(tag, 'tag')"
         class="tags"
       >
       {{tag.tag}}
@@ -24,7 +24,33 @@
         @blur="handleInputConfirm"
       >
       </el-input>
-      <el-button v-show="!inputVisible" class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+      <el-button v-show="!inputVisible" class="button-new-tag" size="small" @click="showInput('tag')">+ New Tag</el-button>
+    </el-card>
+    <el-card class="content-card">
+      <div slot="header" class="clearfix">
+        <span class="card-title">分类管理</span>
+      </div>  
+      <el-tag
+        :key="type"
+        v-for="type in typeList"
+        :closable="true"
+        :close-transition="false"
+        @close="handleClose(type, 'type')"
+        class="tags"
+      >
+      {{type.type}}
+      </el-tag>
+      <el-input
+        class="input-new-tag"
+        v-show="inputTypeVisible"
+        v-model="inputValue"
+        ref="saveTypeInput"
+        size="mini"
+        @keyup.enter.native="handleTypeInputConfirm"
+        @blur="handleTypeInputConfirm"
+      >
+      </el-input>
+      <el-button v-show="!inputTypeVisible" class="button-new-tag" size="small" @click="showInput('type')">+ New Tag</el-button>
     </el-card>
   </div>
 </template>
@@ -36,18 +62,17 @@ export default {
   data () {
     return {
       tagList: [],
-      meta: {
-        page: 1,
-        limit: 25
-      },
+      typeList: [],
       inputValue: '',
-      inputVisible: false
+      inputVisible: false,
+      inputTypeVisible: false
     };
   },
 
   beforeRouteEnter (to, from, next) {
     next(vm => {
       vm.getTagList();
+      vm.getTypeList();
     })
   },
 
@@ -59,34 +84,73 @@ export default {
         this.$toast(r.message);
       })
     },
-    handleClose(tag) {
-      post('tag/delete', { id: tag._id }).then(r=>{
-        this.$toast('删除成功！');
-        this.tagList.splice(this.tagList.indexOf(tag), 1);
+
+    getTypeList() {
+      get('type').then(r=>{
+        this.typeList = r.data;
       }).catch(r=>{
         this.$toast(r.message);
       })
     },
 
-    showInput() {
-      this.inputVisible = true;
-      this.$nextTick(_ => {
-        this.$refs.saveTagInput.$refs.input.focus();
-      });
+    handleClose(tag, type) {
+      const list = type === 'tag' ? this.tagList : this.typeList;
+
+      post( type + '/delete', { id: tag._id }).then(r=>{
+        this.$toast('删除成功！');
+        list.splice(list.indexOf(tag), 1);
+      }).catch(r=>{
+        this.$toast(r.message);
+      })
+    },
+
+    showInput(type) {
+      if(type === 'tag') {
+        this.inputVisible = true;
+        this.$nextTick(_ => {
+          this.$refs.saveTagInput.$refs.input.focus();
+        });
+      } else {
+        this.inputTypeVisible = true;
+        this.$nextTick(_ => {
+          this.$refs.saveTypeInput.$refs.input.focus();
+        });
+      }
     },
 
     handleInputConfirm() {
       let inputValue = this.inputValue;
-      if (!inputValue) {
-        return;
-      }
 
       this.inputVisible = false;
+      this.inputTypeVisible = false;
       this.inputValue = '';
 
+      if (!inputValue) {
+        return true;
+      }
+      
       post('tag/add', { tag: inputValue }, true).then(r=>{
         this.$toast('添加成功！');
         this.tagList.push(r.data);
+      }).catch(r=>{
+        this.$toast(r.message);
+      })
+    },
+
+    handleTypeInputConfirm() {
+      let inputValue = this.inputValue;
+
+      this.inputVisible = false;
+      this.inputTypeVisible = false;
+      this.inputValue = '';
+
+      if (!inputValue) {
+        return true;
+      }
+      
+      post('type/add', { type: inputValue }, true).then(r=>{
+        this.$toast('添加成功！');
+        this.typeList.push(r.data);
       }).catch(r=>{
         this.$toast(r.message);
       })
@@ -102,5 +166,9 @@ export default {
 
 .input-new-tag{
   max-width: 100px;
+}
+
+.content-card{
+  margin-bottom: 40px;
 }
 </style>
