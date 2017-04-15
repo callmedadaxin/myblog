35b2bfb9614544fbd821b5f6e5b3066c8f886355
callmedadaxin <554091944@qq.com>
2017-04-15 23:47:33 +0800

    add desc add

--- frontend/admin/src/views/posts/add/index.vue
+++ frontend/admin/src/views/posts/add/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="main-content">
-    <el-card class="content-card content-edit">
+    <el-card class="content-card content-edit" v-if="!editDesc">
       <div slot="header" class="clearfix">
         <span class="card-title">{{title}}</span>
-        <el-button style="float: right;" type="primary">发布文章</el-button>
+        <el-button style="float: right;" type="primary" @click="goNext">下一步</el-button>
       </div> 
       <div class="edit-wrap">
         <div class="left">
@@ -14,6 +14,43 @@
         </div>
       </div> 
     </el-card>
+    <el-card class="content-card content-edit" v-else>
+      <div slot="header" class="clearfix">
+        <span class="card-title">{{title}}</span>
+        <el-button style="float: right;" type="primary">完成</el-button>
+      </div> 
+      <el-form ref="form" :model="descData" label-width="100px">
+        <el-form-item label="文章标题">
+          <el-input v-model="descData.title"></el-input>
+        </el-form-item>
+        <el-form-item label="文章摘要">
+          <el-input v-model="descData.abstract" type="textarea" placeholder="请输入文章摘要"></el-input>
+        </el-form-item>
+        <el-form-item label="文章配图">
+          
+        </el-form-item>
+        <el-form-item label="标签">
+          <el-select
+            v-model="descData.tag"
+            multiple
+            filterable
+            allow-create
+            placeholder="请选择文章标签">
+            <el-option
+              v-for="item in tagList"
+              :label="item.tag"
+              :value="item._id">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="分类">
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -21,11 +58,22 @@
 import 'common/scss/markdown';
 
 import VueMarkdown from 'vue-markdown';
+import { get, post } from 'common/js/api';
 
 export default {
   data () {
     return {
       source: 'sdfsdf ',
+      editDesc: false, //是否编辑信息
+      descData: {
+        title: '',
+        abstract: '',
+        show: '',
+        tag: [],
+        type: ''
+      },
+      tagList: [],
+      typeList: []
     };
   },
 
@@ -39,10 +87,37 @@ export default {
     }
   },
 
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      vm.getTagList();
+      vm.getTypeList();
+    })
+  },
+
   methods: {
     onScroll(e) {
       this.$refs.preview.scrollTop = e.target.scrollTop;
-    }
+    },
+
+    goNext() {
+      this.editDesc = true;
+    },
+
+    getTagList() {
+      get('tag').then(r=>{
+        this.tagList = r.data;
+      }).catch(r=>{
+        this.$toast(r.message);
+      })
+    },
+
+    getTypeList() {
+      get('type').then(r=>{
+        this.typeList = r.data;
+      }).catch(r=>{
+        this.$toast(r.message);
+      })
+    },
   },
 
   components: {
